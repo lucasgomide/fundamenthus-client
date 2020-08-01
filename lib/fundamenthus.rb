69d898ebe866502747cbe28c26942bd77b71df28
lucasgomide <lucaslg200@gmail.com>
2020-08-01 12:23:23 -0300

    Initial commit

---
+++ lib/fundamenthus.rb
@@ -0,0 +1,6 @@
+require "fundamenthus/version"
+
+module Fundamenthus
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/fundamenthus/version.rb
@@ -0,0 +1,3 @@
+module Fundamenthus
+  VERSION = "0.1.0"
+end
