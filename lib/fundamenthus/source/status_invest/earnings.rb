56708347a0b02d18aa101604752a232c09c067c1
lucasgomide <lucaslg200@gmail.com>
2020-08-18 19:00:00 -0300

    Collect earnings data from StatusInvest

--- lib/fundamenthus/source/status_invest/earnings.rb
+++ lib/fundamenthus/source/status_invest/earnings.rb
@@ -2,9 +2,50 @@ module Fundamenthus
   module Source
     module StatusInvest
       class Earnings
+        attr_accessor :client
+
+        MAX_PAGE = 8
+
+        def initialize(client = Client.new)
+          @client = client
+        end
+
         def collect
-          Fundamenthus.logger.info(__method__) { 'TODO: Crawler not implemented' }
-          []
+          results = []
+          (1..MAX_PAGE).each do |page|
+            response = @client.stock_links(page)
+
+            break if response.status.to_i != 200
+
+            JSON.parse(response.body_str).each do |company|
+              print "\r#{page} pages parsed - #{results.count} earnings fetched"
+              response = @client.stock_page(company['url'])
+              results += parse_earnings(response, company)
+            end
+          rescue StandardError => e
+            puts e.message
+            next
+          end
+          results
+        end
+
+        private
+
+        def parse_earnings(response, company)
+          doc = Nokogiri::HTML(response.body)
+          input = doc.css("#earning-section > input#results").first
+          earnings = JSON.parse(input.attr(:value))
+          stick = company['url'].split('/').last.upcase
+          earnings.map do |r|
+            {
+              valor: r['v'],
+              data_com: r['ed'],
+              pagamento_data: r['pd'],
+              tipo: r['et'],
+              codigo: stick,
+              empresa: company['companyName']
+            }
+          end
         end
       end
     end
--- spec/fundamenthus/source/status_invest/earnings_spec.rb
+++ spec/fundamenthus/source/status_invest/earnings_spec.rb
@@ -1,9 +1,13 @@
-RSpec.describe Fundamenthus::Source::StatusInvest::Earnings do
+RSpec.describe Fundamenthus::Source::StatusInvest::Earnings, :vcr do
   subject(:earnings) { described_class.new }
 
   context '.collect' do
-    subject(:collect) { earnings.collect }
+    let(:earning_data) { load_json_file('sources/status_invest/earnings.json') }
+    let(:indexes) { (0..earning_data.count).to_a.sample(400) }
+    let(:expected_result) { earning_data.values_at(*indexes) }
 
-    it { is_expected.to be_empty }
+    subject(:collect) { JSON.parse(earnings.collect.values_at(*indexes).to_json) }
+
+    it { is_expected.to match_array(expected_result) }
   end
 end
